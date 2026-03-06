7fde74b224471a1bf844c6ea488a3d496363b8ac
Robert Shevchenko <robert.shev@icloud.com>
2026-03-06 10:43:19 +0200

    Add Kontrolltoo project and exception handling

---
+++ 05_06.03-kontrolltoo1/kontroltöö/src/main/java/ee/robert/kontroltoo/KontroltooApplication.java
@@ -0,0 +1,13 @@
+package ee.robert.kontroltoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KontroltooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KontroltooApplication.class, args);
+	}
+
+}
---
+++ 05_06.03-kontrolltoo1/kontroltöö/src/test/java/ee/robert/kontroltoo/KontroltooApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.robert.kontroltoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KontroltooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
